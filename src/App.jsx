b6d7e8f74e6ac3bad024013f35a7eef1a9428d0e
Skund Kumar <brando.skund@gmail.com>
2024-07-18 22:45:04 +0530

    commit-7

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
         <h1 className=' flex justify-center text-red-100 text-[8vw] mb-[1vh] '>ToDo List</h1>
         <input onChange={handleChange}  className='text-zinc-200 px-2 w-[50vw] flex bg-zinc-800 border-double border-4 border-red-300  h-15 rounded-xl' placeholder='  Write new task.....' type="text" />
         <motion.button whileHover={{scale:0.98}}
-                      whileTap={{scale: 2}} onClick={handleClick} className=' bg-red-300/90 text-zinc-800 font-semibold w-full h-12 rounded-xl mt-[2vh]' type="button">Submit</motion.button>
+                      whileTap={{scale: 1}} onClick={handleClick} className=' bg-red-300/90 text-zinc-800 font-semibold w-full h-12 rounded-xl mt-[2vh]' type="button">Submit</motion.button>
         <ul className='mt-5'>
           {todo.map((item,index)=>{
            return <li  key={index} className='bg-red-200 w-fit text-zinc-800 m-2 p-1.5 font-semibold rounded-lg'> {arrow} {item}</li>
